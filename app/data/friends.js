42d5ae87320042985703c9a2ed374924d6b5758f
andrwjrdn <andrewjordanreyes@gmail.com>
2019-05-22 10:31:10 -0700

    server.js fixed

---
+++ app/data/friends.js
@@ -0,0 +1,75 @@
+// DATA FOR APP USE
+
+var friends = [
+  {
+    routeName: 'kobeBryant',
+    name: 'Kobe Bryant',
+    picture: 'https://www.denverpost.com/wp-content/uploads/2017/12/kobe-bryant.jpg?w=541',
+    scores: 
+    [   
+      5,
+      3,
+      2,
+      4,
+      5,
+      1,
+      2,
+      3,
+      4,
+      2
+    ],
+  }, {
+    routeName: "michaelJordan",
+    name: 'Michael Jordan',
+    picture: 'https://sportshub.cbsistatic.com/i/r/2018/02/28/26c9fda0-6a42-4dda-802a-1544acb727ca/thumbnail/770x433/6c3c706de5c2644d34f912aa5dcbcdf3/michael-jordan-bulls.jpg',
+    scores: 
+    [
+      1,
+      2,
+      3,
+      5,
+      2,
+      3,
+      1,
+      5,
+      4,
+      4
+    ]
+  },  {
+    routeName: 'stephCurry',
+    name: 'Steph Curry',
+    picture: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpeopledotcom.files.wordpress.com%2F2018%2F11%2Fgettyimages-1064769920_1.jpg%3Fw%3D2000&w=450&c=sc&poi=face&q=85',
+    scores: 
+    [   
+      2,
+      3,
+      5,
+      4,
+      1,
+      1,
+      4,
+      3,
+      4,
+      2
+    ],
+  }, {
+    routeName: "garyPayton",
+    name: 'Gary Payton',
+    picture: 'https://www.gannett-cdn.com/-mm-/8177e1eeb06a934d4135ff6c395a147b5d96b563/c=0-261-1148-1124/local/-/media/USATODAY/USATODAY/2013/04/05/xxx-nba-playoffs_-lakers_sonics.jpg?width=534&height=401&fit=crop',
+    scores: 
+    [
+      1,
+      2,
+      3,
+      5,
+      2,
+      3,
+      1,
+      5,
+      4,
+      4
+    ]
+  },
+];
+
+module.exports = matches; 
