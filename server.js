ddd175a2efc27ee0d8b8346062a17b404f3fee1d
andrwjrdn <andrewjordanreyes@gmail.com>
2019-05-22 12:21:32 -0700

    edited apiroute var

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 //now we save data in friends.js
 //friends.js will be used in routes js files
 //and we need to import those routes files
-var apiroutes = require("./app/routing/apiroutes.js");
+var apiroutes = require("./app/routing/apiRoutes.js");
 var htmlroutes = require("./app/routing/htmlroutes.js");
 
 app.use(apiroutes);
